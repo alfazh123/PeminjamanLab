838f92632e9a781219b8057b4f5f34c98a8f0129
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-11-30 14:44:21 +0700

    new project

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <main>
+            <div className="">
+                <h1>halo</h1>
+            </div>
+            <div>
+                <h1>hahhahah</h1>
+            </div>
+        </main>
+    )
+}
---
+++ app/ui/navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export default function Navbar() {
+    return (
+        <div className="flex flex-col border-solid border-slate-600 ">
+            <div className="bg-blue-500 p-6 rounded-md">
+                <Link href="/">Lebolatory</Link>
+            </div>
+
+            <div className="flex justify-evenly items-center">
+                <Link className="px-20 py-4 rounded-sm" href="/dashboard">Dashboard</Link>
+                <Link className="px-20 py-4 rounded-sm" href="/dashboard/peminjaman">Peminjaman</Link>
+            </div>
+        </div>
+    )
+}
