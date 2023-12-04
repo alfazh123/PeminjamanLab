05e000fab4fde63674c9b0e19ac4a2c7ebac4acb
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-04 08:54:11 +0700

    try call students data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+const getStudents = async() => {
+    const res = await prisma.students.findMany({
+        select: {
+            NIM: true,
+            Name: true,
+            Email: true,
+        }
+    })
+    return res
+}
 
 export default async function Page() {
+    const students = await getStudents()
+    console.log(students)
     return (
         <main>
-            <div className="">
-                <h1>halo</h1>
-            </div>
-            <div>
-                <h1>hahhahah</h1>
-            </div>
+            <h1 className="text-3xl font-bold">Mahasiswa</h1>
+            <table className="w-full text-center">
+                <thead className="bg-gray-200 rounded-md">
+                    <tr className="p-5">
+                        <th>NIM</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {students.map((students, index) => (
+                        <tr key={index}>
+                            <td>{students.NIM}</td>
+                            <td>{students.Name}</td>
+                            <td>{students.Email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </main>
     )
 }
